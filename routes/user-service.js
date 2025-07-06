801caf84447038d2ca7906476217d9ca817883ef
Bryan03A <hacker03291997@gmail.com>
2025-07-06 11:29:59 -0500

    feat: :fire: Correct routing in catalog service

--- routes/user-service.js
+++ routes/user-service.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
-const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://50.19.4.172/user-soap';
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || 'http://3.224.44.87/user-soap';
 
 // Route to handle new user registration
 router.post('/register', async (req, res) => {
